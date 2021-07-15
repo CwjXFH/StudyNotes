1ab616cf9c15dcb45d0666389aa56ff45da5d233
eason <chiwenjun@cxist.com>
2021-07-15 16:22:38 +0800

    更新：博客异步与协程 & .NET 面试题  & 增加web框架图

--- Blogs/异步与协程/demo/demo.js
+++ Blogs/异步与协程/demo/demo.js
@@ -2,7 +2,7 @@
 // 调用async函数会返回Promise对象
 // 遇到await表达式时，会暂停当前协程的执行，等待await表达式运算完成
 // await表达式：
-//      1. 如果await后面是Promise对象，则当Promise对象的状态为fulfill时（即调用resolve 方法），
+//      1. 如果await后面是Promise对象，则当Promise对象的状态为fulfill/reject时，
 //      await表达式结束等待，await后面的代码将被执行
 //
 //      2. 如果await后面不是Promise对象，则隐式包装为状态为fulfill的Promise对象
@@ -20,15 +20,11 @@ async function PromiseAll(values) {
         });
     }
 
-    // function returnResult() {
     // console.log('waiting result');
     if (result.length == values.length) {
         // console.log('promise all result', result);
         return result;
     }
-    // }
-
-    // return returnResult();
 }
 
 
@@ -51,11 +47,11 @@ async function func2() {
 (async function () {
     console.log('before await');
     let result = [];
-    // result = await PromiseAll([func1(), func2()]);
+    result = await PromiseAll([func1(), func2()]);
     // result = await Promise.all([func1(), func2()]);
 
-    result[0] = await func1();
-    result[1] = await func2();
+    // result[0] = await func1();
+    // result[1] = await func2();
     // console.log(`func1() instanceof Promise: ${func1() instanceof Promise}`);
     // func2();
     console.log('after await');
---
+++ Blogs/异步与协程/demo/generator.js
@@ -0,0 +1,44 @@
+function* promiseAll(values) {
+    for (let i = 0; i < values.length; i++) {
+        // yield values[i];
+        yield new Promise(resolve => {
+            setTimeout(() => resolve(values[i]), 3000);
+        }).then(value => {
+            console.log(`func1: ${value}`);
+            return value;
+        });
+    }
+}
+
+function func1() {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(100), 3000);
+    }).then(value => {
+        console.log(`func1: ${value}`);
+        return value;
+    });
+}
+
+function func2() {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(200), 3000);
+    }).then(value => {
+        console.log(`func2: ${value}`);
+        return value;
+    });
+}
+
+// let all = promiseAll([func1(), func2()]);
+// let p1 = all.next();
+// func1();
+// func2();
+// setTimeout(() => console.log('over'), 2000);
+// new Promise(resolve => {
+//     setTimeout(() => resolve(100), 2000);
+// })
+//     .then(value => console.log(value));
+
+let all = promiseAll([100, 200]);
+let p1 = all.next();
+all.next();
+console.log();
